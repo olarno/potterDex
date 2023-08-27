f3fc12214e3bedae30ba0b64b9c16f9e78af2c25
Ol arno <arnaudoltra@hotmail.fr>
2023-08-26 23:04:22 -0400

    Remove unneccessary data

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,8 +20,9 @@ const app = {
   loadCharacters: function () {
     fetch(app.apiBaseURL + 'characters', app.fetchOption)
       .then(function (response) {
-        if (response.ok)
+        if (response.ok){
           return response.json();
+        }
         else
           alert('Une erreur est survenue lors du chargement des personnages : ' + response.status + ' ' + response.statusText);
       })
@@ -42,7 +43,7 @@ const app = {
   },
 
   selectOneChars: function (evt) {
-    console.log(evt.currentTarget.value);
+    // console.log(evt.currentTarget.value);
     app.review.innerHTML = '';
     fetch(app.apiBaseURL + 'character/' + evt.currentTarget.value, app.fetchOption)
       .then(function (response) {
@@ -59,84 +60,85 @@ const app = {
 
   onlyOneChar: function (char) {
 
+
     let charReviewTemplate = app.charTemplate.content.cloneNode(true);
     let character = charReviewTemplate.querySelector('#character');
-    console.log(character);
+    console.log(char);
 
     let name = charReviewTemplate.querySelector('.name');
     name.textContent = char[0].name;
 
     let species = charReviewTemplate.querySelector('.species');
-    if (char[0].species !== 'undefined')
+    if (char[0].species)
       species.innerHTML += char[0].species;
     else
       species.classList.add('false');
 
     let gender = charReviewTemplate.querySelector('.gender');
-    if (char[0].gender !== 'undefined')
+    if (char[0].gender)
       gender.innerHTML += char[0].gender;
     else
       gender.classList.add('false');
 
     let house = charReviewTemplate.querySelector('.house');
-    if (typeof char[0].house !== 'undefined')
+    if (char[0].house)
       house.innerHTML += char[0].house;
     else
       house.classList.add('false');
 
     let patronus = charReviewTemplate.querySelector('.patronus');
-    if (typeof char[0].patronus !== 'undefined')
+    if (char[0].patronus)
       patronus.innerHTML += char[0].patronus;
     else
       patronus.classList.add('false');
 
     let dateOfBirth = charReviewTemplate.querySelector('.dateOfBirth');
-    if (typeof char[0].dateOfBirth !== 'undefined')
+    if (char[0].dateOfBirth)
       dateOfBirth.innerHTML += char[0].dateOfBirth;
     else
       dateOfBirth.classList.add('false');
 
     let yearOfBirth = charReviewTemplate.querySelector('.yearOfBirth');
-    if (typeof char[0].yearOfBirth !== 'undefined')
+    if (char[0].yearOfBirth)
       yearOfBirth.innerHTML += char[0].yearOfBirth;
     else
       yearOfBirth.classList.add('false');
 
     let ancestry = charReviewTemplate.querySelector('.ancestry');
-    if (typeof char[0].ancestry !== 'undefined')
+    if (char[0].ancestry)
       ancestry.innerHTML += char[0].ancestry;
     else
       ancestry.classList.add('false');
 
     let eyeColour = charReviewTemplate.querySelector('.eyeColour');
-    if (typeof char[0].eyeColour !== 'undefined')
+    if (char[0].eyeColour) 
       eyeColour.innerHTML += char[0].eyeColour;
     else
       eyeColour.classList.add('false');
 
     let hairColour = charReviewTemplate.querySelector('.hairColour');
-    if (typeof char[0].hairColour !== 'undefined')
+    if (char[0].hairColour)
       hairColour.innerHTML += char[0].hairColour;
     else
       hairColour.classList.add('false');
 
-    let picture = charReviewTemplate.querySelector('.img-thumbnail');
+    let picture = charReviewTemplate.querySelector('.profile_picture');
     picture.src = char[0].image;
 
     let wood = charReviewTemplate.querySelector('.wood');
-    if (typeof char[0].wand['wood'] !== 'undefined')
+    if (char[0].wand['wood'])
       wood.innerHTML += char[0].wand['wood'];
     else
       wood.classList.add('false');
 
     let core = charReviewTemplate.querySelector('.core');
-    if (typeof char[0].wand['core'] !== 'undefined')
+    if (char[0].wand['core'])
       core.innerHTML += char[0].wand['core'];
     else
       core.classList.add('false');
 
     let length = charReviewTemplate.querySelector('.length');
-    if (typeof char[0].wand['length'] !== 'undefined')
+    if (char[0].wand['length'])
       length.innerHTML += char[0].wand['length'];
     else
       length.classList.add('false');
